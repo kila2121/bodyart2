aa4725ab880b3a4aec264c37821359cd1510b035
kila2121 <xaa-1artem@yandex.ru>
2026-03-25 19:06:29 +0300

    update_style_for_user_master_admin

--- component/details_master_component/reviews/reviews.php
+++ component/details_master_component/reviews/reviews.php
@@ -4,40 +4,36 @@
             <i class="fas fa-star"></i>
             Отзывы клиентов
         </h2>
-        <a href="/page/reviews.php?master=<?= $master['id'] ?>" class="section-link">
-            Все отзывы <i class="fas fa-arrow-right"></i>
-        </a>
     </div>
     <div class="reviews-list">
-        <?php foreach($reviews as $review): 
+        <?php foreach ($reviews as $review):
             $reviewName = $review['login'] ?: ($review['fio'] ?: 'Гость');
             $firstLetter = mb_strtoupper(mb_substr($reviewName, 0, 1));
             $colors = ['#ff3366', '#10b981', '#f59e0b', '#8b5cf6'];
             $color = $colors[array_rand($colors)];
-        ?>
-        <div class="review-item">
-            <div class="review-header">
-                <div class="review-author">
-                    <?php if (!empty($review['avatar_url'])): ?>
-                    <img src="<?= htmlspecialchars($review['avatar_url']) ?>" 
-                            alt="<?= htmlspecialchars($reviewName) ?>"
-                            class="review-avatar-img">
-                    <?php else: ?>
-                    <div class="review-avatar" style="background: <?= $color ?>;">
-                        <?= $firstLetter ?>
+            ?>
+            <div class="review-item">
+                <div class="review-header">
+                    <div class="review-author">
+                        <?php if (!empty($review['avatar_url'])): ?>
+                            <img src="<?= htmlspecialchars($review['avatar_url']) ?>" alt="<?= htmlspecialchars($reviewName) ?>"
+                                class="review-avatar-img">
+                        <?php else: ?>
+                            <div class="review-avatar" style="background: <?= $color ?>;">
+                                <?= $firstLetter ?>
+                            </div>
+                        <?php endif; ?>
+                        <span class="review-name"><?= htmlspecialchars($reviewName) ?></span>
                     </div>
-                    <?php endif; ?>
-                    <span class="review-name"><?= htmlspecialchars($reviewName) ?></span>
+                    <div class="review-rating">
+                        <?php for ($i = 1; $i <= 5; $i++): ?>
+                            <i class="fas fa-star<?= $i <= $review['rating'] ? '' : '-o' ?>"></i>
+                        <?php endfor; ?>
+                    </div>
                 </div>
-                <div class="review-rating">
-                    <?php for($i = 1; $i <= 5; $i++): ?>
-                        <i class="fas fa-star<?= $i <= $review['rating'] ? '' : '-o' ?>"></i>
-                    <?php endfor; ?>
-                </div>
+                <p class="review-text"><?= htmlspecialchars(mb_substr($review['comment'], 0, 150)) ?>...</p>
+                <span class="review-date"><?= date('d.m.Y', strtotime($review['created_at'])) ?></span>
             </div>
-            <p class="review-text"><?= htmlspecialchars(mb_substr($review['comment'], 0, 150)) ?>...</p>
-            <span class="review-date"><?= date('d.m.Y', strtotime($review['created_at'])) ?></span>
-        </div>
         <?php endforeach; ?>
     </div>
 </div>
