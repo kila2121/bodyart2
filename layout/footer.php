c2ca3f0037f1f5ee3e090a4a838da6db77c00a00
kila2121 <xaa-1artem@yandex.ru>
2026-04-10 21:15:43 +0300

    style footer

--- layout/footer.php
+++ layout/footer.php
@@ -3,7 +3,8 @@
 		<div class="footer_up">
 			<div class="footer_col">
 				<h4 class="footer_title">BodyArt Studio</h4>
-				<p class="footer_description">Премиальная студия модификации тела с 2016 года. Искусство, качество и безопасность.</p>
+				<p class="footer_description">Премиальная студия модификации тела с 2016 года. Искусство, качество и
+					безопасность.</p>
 				<div class="footer_social">
 					<a href="#" class="social_link"><i class="fab fa-instagram"></i></a>
 					<a href="#" class="social_link"><i class="fab fa-vk"></i></a>
@@ -14,34 +15,32 @@
 			<div class="footer_col">
 				<h4 class="footer_title">Услуги</h4>
 				<ul class="footer_links">
-					<li><a href='/services#tattoo' class="info"><i class="fas fa-chevron-right"></i>Художественные тату</a></li>
-					<li><a href="/services#piercing" class="info"><i class="fas fa-chevron-right"></i>Пирсинг</a></li>
-					<li><a href="/services#biomod" class="info"><i class="fas fa-chevron-right"></i>Биомодификации</a></li>
-					<li><a href="/services#care" class="info"><i class="fas fa-chevron-right"></i>Уход и коррекция</a></li>
+					<li><a href="/index.php?page=services" class="info"><i class="fas fa-chevron-right"></i>Все
+							услуги</a></li>
 				</ul>
 			</div>
 			<div class="footer_col">
 				<h4 class="footer_title">Информация</h4>
 				<ul class="footer_links">
-					<li><a href='/#aboutUs' class="info"><i class="fas fa-chevron-right"></i>О студии</a></li>
-					<li><a href="/masters" class="info"><i class="fas fa-chevron-right"></i>Мастера</a></li>
-					<li><a href="#" class="info"><i class="fas fa-chevron-right"></i>Цены</a></li>
-					<li><a href="/blog" class="info"><i class="fas fa-chevron-right"></i>Блог</a></li>
-					<li><a href="#" class="info"><i class="fas fa-chevron-right"></i>FAQ</a></li>
+					<li><a href="/#aboutUs" class="info"><i class="fas fa-chevron-right"></i>О студии</a></li>
+					<li><a href="/index.php?page=masters" class="info"><i class="fas fa-chevron-right"></i>Мастера</a>
+					</li>
+					<li><a href="/index.php?page=gallery" class="info"><i class="fas fa-chevron-right"></i>Галерея</a>
+					</li>
 				</ul>
 			</div>
 			<div class="footer_col">
 				<h4 class="footer_title">Контакты</h4>
 				<div class="footer_contact">
-					<p class="info">Москва, ул. Арбат, 25</p>
-					<p class="info">+7 (495) 123-45-67</p>
-					<p class="info">info@bodyart-studio.ru</p>
+					<p class="info">Ставрополь, ул. Ленина, 301</p>
+					<p class="info">+7 (999) 123-45-67</p>
+					<p class="info">bodyart-studio@yandex.ru</p>
 				</div>
 			</div>
 		</div>
 		<div class="footer_divider"></div>
 		<div class="footer_bottom">
-			<p>© 2025 BodyArt Studio. Все права защищены.</p>
+			<p>© 2016 BodyArt Studio. Все права защищены.</p>
 			<div class="footer_pay">
 				<a><i class="fab fa-cc-visa"></i></a>
 				<a><i class="fab fa-cc-mastercard"></i></a>
